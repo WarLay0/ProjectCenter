5ac70f5ee1bc6db431a55d4ce37737032c3e792d
carine <105859@ecole-it.com>
2026-04-13 23:10:56 +0200

    page auth

--- app/frontend/src/app/features/auth/pages/login/login.component.ts
+++ app/frontend/src/app/features/auth/pages/login/login.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+   imports: [FormsModule]
 })
 export class LoginComponent {
 
+  email = '';
+  password = '';
+  error = '';
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  onSubmit() {
+    this.authService.login(this.email, this.password).subscribe({
+      next: () => {
+        this.router.navigate(['/projects']);
+      },
+      error: () => {
+        this.error = 'Identifiants invalides';
+      }
+    });
+  }
 }
--- app/frontend/src/app/features/auth/services/auth.service.ts
+++ app/frontend/src/app/features/auth/services/auth.service.ts
@@ -1,9 +1,45 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../../environments/environment';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  login(email: string, password: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/api/login_check`, {
+      username: email,
+      password: password
+    }).pipe(
+      tap((res: any) => {
+        this.setToken(res.token);
+      })
+    );
+  }
+
+  getMe(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/api/me`);
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+  }
+
+  setToken(token: string) {
+    localStorage.setItem('token', token);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem('token');
+  }
+
+  isLogged(): boolean {
+    return !!this.getToken();
+  }
 }
